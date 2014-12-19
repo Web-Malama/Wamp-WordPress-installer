90f1a786c64b754b0fa64f8fd9b358b5dbb735c4
Web-Malama <aaron@webmalama.com>
2014-12-19 11:20:40 -1000

    Update installer.php
    Replaced the http_get_file function that uses cURL with a new function, get_latest_version, which instead uses file_get_contents. Also changed URI's from http to https. Added minor error reporting when failure occurs getting latest version.

--- installer.php
+++ installer.php
@@ -19,12 +19,12 @@ $versions = array(
   ),
   array(
     'label' => 'Latest version',
-    'url' => 'http://wordpress.org/latest.zip',
+    'url' => 'https://wordpress.org/latest.zip',
     'description' => 'Download the latest stable English version'
   ),
   array(
     'label' => 'Last nightly',
-    'url' => 'http://wordpress.org/nightly-builds/wordpress-latest.zip',
+    'url' => 'https://wordpress.org/nightly-builds/wordpress-latest.zip',
     'description' => 'Download the latest beta version'
   ),
   array(
@@ -173,17 +173,23 @@ $done_zip = false;
 if (isset($_POST['install'])){
   //download latest
   if (isset($_POST['ver']) && in_array($_POST['ver'],array('1','2'))){
-    $download_ok = http_get_file($versions[$_POST['ver']]['url'],'latest.zip');
+    $download_ok = get_latest_version($versions[$_POST['ver']]['url']);
+	if(!$download_ok){
+		echo'<br/> Could not download the latest version at '.$versions[$_POST['ver']]['url'].'.';
+	}
   }elseif ($_POST['ver'] == '0'){
     //use local file
     $download_ok = file_exists('latest.zip');
     if(!$download_ok){
-      echo '<br/> File dosen\'t exists, try downloading the latest version.';
+      echo '<br/> File doesn\'t exists, try downloading the latest version.';
     }
   }else{
     //use get latest stable url for remote
     $uri = Get_latest_version_url($versions[$_POST['ver']]['lang_code']);
-    $download_ok = http_get_file($uri,'latest.zip');
+    $download_ok = get_latest_version($uri);
+	if(!$download_ok){
+		echo'<br/> Could not download the latest version at '.$uri.'.';
+	}
   }
   //unzip
   if ($download_ok){
@@ -253,14 +259,12 @@ if (isset($_POST['install'])){
     <title>WordPress Installer For WAMP</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <style type="text/css">
-    html{background:#f9f9f9;}body{background:#fff;color:#333;font-family:sans-serif;margin:2em auto;padding:1em 2em;-webkit-border-radius:3px;border-radius:3px;border:1px solid #dfdfdf;max-width:700px;}a{color:#21759B;text-decoration:none;}a:hover{color:#D54E21;}h1{border-bottom:1px solid #dadada;clear:both;color:#666;font:24px Georgia,"Times New Roman",Times,serif;margin:30px 0 0 0;padding:0;padding-bottom:7px;}h2{font-size:16px;}p,li,dd,dt{padding-bottom:2px;font-size:14px;line-height:1.5;}code,.code{font-size:14px;}ul,ol,dl{padding:5px 5px 5px 22px;}a img{border:0;}abbr{border:0;font-variant:normal;}#logo{margin:6px 0 14px 0;border-bottom:none;text-align:center;}.step{margin:20px 0 15px;}.step,th{text-align:left;padding:0;}.submit input,.button,.button-secondary{font-family:sans-serif;text-decoration:none;font-size:14px!important;line-height:16px;padding:6px 12px;cursor:pointer;border:1px solid #bbb;color:#464646;-webkit-border-radius:15px;border-radius:15px;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box;}.button:hover,.button-secondary:hover,.submit input:hover{color:#000;border-color:#666;}.button,.submit input,.button-secondary{background:#f2f2f2 url(../images/white-grad.png) repeat-x scroll left top;}.button:active,.submit input:active,.button-secondary:active{background:#eee url(../images/white-grad-active.png) repeat-x scroll left top;}textarea{border:1px solid #dfdfdf;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;width:695px;}.form-table{border-collapse:collapse;margin-top:1em;width:100%;}.form-table td{margin-bottom:9px;padding:10px 20px 10px 0;border-bottom:8px solid #fff;font-size:14px;vertical-align:top;}.form-table th{font-size:14px;text-align:left;padding:16px 20px 10px 0;border-bottom:8px solid #fff;width:140px;vertical-align:top;}.form-table code{line-height:18px;font-size:14px;}.form-table p{margin:4px 0 0 0;font-size:11px;}.form-table input{line-height:20px;font-size:15px;padding:2px;border:1px #DFDFDF solid;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;}.form-table input[type=text],.form-table input[type=password]{width:206px;}.form-table th p{font-weight:normal;}.form-table.install-success td{vertical-align:middle;padding:16px 20px 10px 0;}.form-table.install-success td p{margin:0;font-size:14px;}.form-table.install-success td code{margin:0;font-size:18px;}#error-page{margin-top:50px;}#error-page p{font-size:14px;line-height:18px;margin:25px 0 20px;}#error-page code,.code{font-family:Consolas,Monaco,monospace;}#pass-strength-result{background-color:#eee;border-color:#ddd!important;border-style:solid;border-width:1px;margin:5px 5px 5px 0;padding:5px;text-align:center;width:200px;display:none;}#pass-strength-result.bad{background-color:#ffb78c;border-color:#ff853c!important;}#pass-strength-result.good{background-color:#ffec8b;border-color:#fc0!important;}#pass-strength-result.short{background-color:#ffa0a0;border-color:#f04040!important;}#pass-strength-result.strong{background-color:#c3ff88;border-color:#8dff1c!important;}.message{border:1px solid #e6db55;padding:.3em .6em;margin:5px 0 15px;background-color:#ffffe0;}body.rtl{font-family:Tahoma,arial;}.rtl h1{font-family:arial;margin:5px -4px 0 0;}.rtl ul,.rtl ol{padding:5px 22px 5px 5px;}.rtl .step,.rtl th,.rtl .form-table th{text-align:right;}.rtl .submit input,.rtl .button,.rtl .button-secondary{margin-right:0;}.rtl #user_login,.rtl #admin_email,.rtl #pass1,.rtl #pass2{direction:ltr;}#description {background-repeat: no-repeat;color: #4C4C4C;font-family: Georgia,"Times New Roman",Times,serif;font-size: 14px;font-style: italic;min-height: 45px;padding: 15px 10px;background: none repeat scroll 0 0 #DBECF8;border-radius: 6px 6px 6px 6px;float: right;}
+    html{background:#f9f9f9;}body{background:#fff;color:#333;font-family:sans-serif;margin:2em auto;padding:1em 2em;-webkit-border-radius:3px;border-radius:3px;border:1px solid #dfdfdf;max-width:700px;}a{color:#21759B;text-decoration:none;}a:hover{color:#D54E21;}h1{border-bottom:1px solid #dadada;clear:both;color:#666;font:24px Georgia,"Times New Roman",Times,serif;margin:30px 0 0 0;padding:0;padding-bottom:7px;}h2{font-size:16px;}p,li,dd,dt{padding-bottom:2px;font-size:14px;line-height:1.5;}code,.code{font-size:14px;}ul,ol,dl{padding:5px 5px 5px 22px;}a img{border:0;}abbr{border:0;font-variant:normal;}#logo{margin:6px 0 14px 0;border-bottom:none;text-align:center;}.step{margin:20px 0 15px;}.step,th{text-align:left;padding:0;}.submit input,.button,.button-secondary{font-family:sans-serif;text-decoration:none;font-size:14px!important;line-height:16px;padding:6px 12px;cursor:pointer;border:1px solid #bbb;color:#464646;-webkit-border-radius:15px;border-radius:15px;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box;}.button:hover,.button-secondary:hover,.submit input:hover{color:#000;border-color:#666;}.button,.submit input,.button-secondary{background:#f2f2f2;}.button:active,.submit input:active,.button-secondary:active{background:#eee url(../images/white-grad-active.png) repeat-x scroll left top;}textarea{border:1px solid #dfdfdf;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;width:695px;}.form-table{border-collapse:collapse;margin-top:1em;width:100%;}.form-table td{margin-bottom:9px;padding:10px 20px 10px 0;border-bottom:8px solid #fff;font-size:14px;vertical-align:top;}.form-table th{font-size:14px;text-align:left;padding:16px 20px 10px 0;border-bottom:8px solid #fff;width:140px;vertical-align:top;}.form-table code{line-height:18px;font-size:14px;}.form-table p{margin:4px 0 0 0;font-size:11px;}.form-table input{line-height:20px;font-size:15px;padding:2px;border:1px #DFDFDF solid;-webkit-border-radius:3px;border-radius:3px;font-family:sans-serif;}.form-table input[type=text],.form-table input[type=password]{width:206px;}.form-table th p{font-weight:normal;}.form-table.install-success td{vertical-align:middle;padding:16px 20px 10px 0;}.form-table.install-success td p{margin:0;font-size:14px;}.form-table.install-success td code{margin:0;font-size:18px;}#error-page{margin-top:50px;}#error-page p{font-size:14px;line-height:18px;margin:25px 0 20px;}#error-page code,.code{font-family:Consolas,Monaco,monospace;}#pass-strength-result{background-color:#eee;border-color:#ddd!important;border-style:solid;border-width:1px;margin:5px 5px 5px 0;padding:5px;text-align:center;width:200px;display:none;}#pass-strength-result.bad{background-color:#ffb78c;border-color:#ff853c!important;}#pass-strength-result.good{background-color:#ffec8b;border-color:#fc0!important;}#pass-strength-result.short{background-color:#ffa0a0;border-color:#f04040!important;}#pass-strength-result.strong{background-color:#c3ff88;border-color:#8dff1c!important;}.message{border:1px solid #e6db55;padding:.3em .6em;margin:5px 0 15px;background-color:#ffffe0;}body.rtl{font-family:Tahoma,arial;}.rtl h1{font-family:arial;margin:5px -4px 0 0;}.rtl ul,.rtl ol{padding:5px 22px 5px 5px;}.rtl .step,.rtl th,.rtl .form-table th{text-align:right;}.rtl .submit input,.rtl .button,.rtl .button-secondary{margin-right:0;}.rtl #user_login,.rtl #admin_email,.rtl #pass1,.rtl #pass2{direction:ltr;}#description {background-repeat: no-repeat;color: #4C4C4C;font-family: Georgia,"Times New Roman",Times,serif;font-size: 14px;font-style: italic;min-height: 45px;padding: 15px 10px;background: none repeat scroll 0 0 #DBECF8;border-radius: 6px 6px 6px 6px;float: right;}
     </style>
   </head>
   <body>
     <div>
-      <h1 id="logo">
-    <img src="http://s.wordpress.org/about/images/logos/wordpress-logo-hoz-rgb.png">
-    </h1>
+      
       <p>Simple WordPress Auto Installer for WAMP ( XAMP and other stacks)</p>
       <p>By Bainternet <a href="http://en.bainternet.info">WordPress Developer</a></p>
       <?php if (!function_exists('curl_init') || !in_array('curl', get_loaded_extensions())){ ?>
@@ -272,7 +276,6 @@ if (isset($_POST['install'])){
     Select Your installation:
     <select name="ver" id="ver">
     <?php
-      $radios = '';
       foreach ($versions as $key => $arr){
         $radios .=  '<option  value="'.$key.'" />'.$arr['label'].'</option>';
       }
@@ -421,6 +424,21 @@ function create_db($user,$pass,$server,$db_name){
 }
   
 /**
+ * get_latest_version function to retrieve the latest version and save it locally
+ *
+ * @author Aaron Brown <aaron@webmalama.com>
+ * @since 0.4
+ * @param (string) $uri The uri to the latest WordPress install file
+ * 
+ * @return (string) Returns either the number of bytes written to file or false
+ */
+function get_latest_version($uri){
+	$file = 'latest.zip';
+	$latest = file_get_contents($uri);
+	return(file_put_contents($file, $latest));
+}
+
+/**
  * Get_latest_version_url function to get the latest version download url
  *
  * @author Ohad Raz <admin@bainternet.info>
@@ -430,7 +448,7 @@ function create_db($user,$pass,$server,$db_name){
  * @return (string)
  */
 function Get_latest_version_url($local = "en_US"){
-  $url =  'http://api.wordpress.org/core/version-check/1.6/?locale='.$local;
+  $url =  'https://api.wordpress.org/core/version-check/1.6/?locale='.$local;
   $c = curl_init($url);
   curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
   $page = curl_exec($c);
@@ -454,6 +472,8 @@ function Get_latest_version_url($local = "en_US"){
  */
 function includejQuery(){
   ?>
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+  <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>-->
+   <script type="text/javascript" src="jquery.js"></script>
   <?php
 }
+?>
